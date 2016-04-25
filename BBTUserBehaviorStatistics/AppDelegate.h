f6de5ea4db624a2e2147bebfe6c66d62f9e83311
JoyHong <hongwanli163@gmail.com>
2016-04-25 17:46:44 +0800

    Initial Commit

---
+++ BBTUserBehaviorStatistics/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTUserBehaviorStatistics
+//
+//  Created by XiaoDou on 4/25/16.
+//  Copyright © 2016 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBTUserBehaviorStatistics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBTUserBehaviorStatistics
+//
+//  Created by XiaoDou on 4/25/16.
+//  Copyright © 2016 北京嗨购电子商务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
